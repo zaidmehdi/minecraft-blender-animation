facb98373518b4f96ed7b0b5ed7f572105c4b751
zaidmehdi <mehdizaid02@gmail.com>
2024-06-24 07:36:16 +0100

    add spinning loader

--- src/front/index.js
+++ src/front/index.js
@@ -1,11 +1,15 @@
 document.getElementById("textForm").addEventListener("submit", function(event) {
     event.preventDefault();
+
+    document.getElementById("loader").style.display = "block";
+
     var userInput = document.getElementById("userInput").value;
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "http://localhost:5000/animate");
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
+            document.getElementById("loader").style.display = "none";
             if (xhr.status === 200) {
                 var response = JSON.parse(xhr.responseText);
                 displayVideo(response.video_path);
